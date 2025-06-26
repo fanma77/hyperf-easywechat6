c908fcff988913f31a1f3801e114c8433ec9c80f
fanma <41362828+fanma77@users.noreply.github.com>
2025-06-26 18:03:20 +0800

    first commit

---
+++ src/EasyWechat.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact group@hyperf.io
+ * @license https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Fanma\HyperfWechat;
+
+use Hyperf\Utils\ApplicationContext;
+
+/**
+ * Class EasyWechat
+ * @method static \EasyWeChat\OfficialAccount\Application officialAccount(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\Pay\Application pay(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\MiniApp\Application miniApp(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\OpenPlatform\Application openPlatform(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\Work\Application work(string $name = "default", array $config = [])
+ * @method static \EasyWeChat\OpenWork\Application openWork(string $name = "default", array $config = [])
+ */
+class EasyWechat
+{
+    public static function __callStatic($functionName, $args)
+    {
+        if (class_exists('\Hyperf\Context\ApplicationContext')) {
+            return \Hyperf\Context\ApplicationContext::getContainer()->get(Factory::class)->{$functionName}(...$args);
+        }
+
+        return ApplicationContext::getContainer()->get(Factory::class)->{$functionName}(...$args);
+    }
+}
---
+++ src/Helper.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact group@hyperf.io
+ * @license https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Fanma\HyperfWechat;
+
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Psr\Http\Message\ResponseInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface as PsrResponseInterface;
+
+class Helper
+{
+    public static function Response(ResponseInterface $response)
+    {
+        if (class_exists('\Hyperf\Context\ApplicationContext')) {
+            $psrResponse = \Hyperf\Context\ApplicationContext::getContainer()->get(PsrResponseInterface::class);
+        } else {
+            $psrResponse = ApplicationContext::getContainer()->get(PsrResponseInterface::class);
+        }
+
+        $psrResponse = $psrResponse->withBody(new SwooleStream((string)$response->getBody()))
+            ->withStatus($response->getStatusCode());
+
+        foreach ($response->getHeaders() as $key => $item) {
+            $psrResponse = $psrResponse->withHeader($key, $item);
+        }
+
+        return $psrResponse;
+    }
+}
